f65839cb07906e621247efbe301d6d675463da2c
achaka119 <askarkajratbek@gmail.com>
2026-01-08 01:08:54 +0500

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,158 @@
+import javax.naming.CompositeName;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
+    private static ArrayList<Member> members = new ArrayList<>();
+    private static ArrayList<Trainer> trainers = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+    private static Membership basic = new Membership(1, "Basic", 30.0, 1, true);
+    private static Membership premium = new Membership(2, "Premium", 60.0, 3, true);
+
     public static void main(String[] args) {
 
-        System.out.println("-----SECTION 1: CREATE MEMBERSHIPS-----");
-        Membership basic = new Membership(1, "Basic", 30.0, 1, true);
-        Membership premium = new Membership(2, "Premium", 60.0, 3, true);
-        System.out.println(basic);
-        System.out.println(premium);
+        boolean running = true;
+        while(running) {
+            displayMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("\n-----SECTION 2: MEMBERSHIP LOGIC-----");
-        System.out.println("Basic total cost: " + basic.totalCost());
-        premium.applyDiscount(10);
-        System.out.println("Premium after discount: " + premium);
-        System.out.println("Premium total cost: " + premium.totalCost());
+            switch (choice) {
+                case 1:
+                    addMember();
+                    break;
+                case 2:
+                    viewAllMembers();
+                    break;
+                case 3:
+                    addTrainer();
+                    break;
+                case 4:
+                    viewAllTrainers();
+                    break;
+                case 0:
+                    System.out.println("\n Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("\n Invalid choice!");
+            }
+            if (running) {
+                System.out.println("\nPress Enter to continue...");
+                scanner.nextLine();
+            }
+        }
+        scanner.close();
+    }
 
-        System.out.println("\n-----SECTION 3: CREATE MEMBER-----");
-        Member m1 = new Member(101, "Aruzhan", 19, true, basic);
-        System.out.println(m1);
-        System.out.println("Can enter gym? " + m1.canEnterGym());
+    private static void displayMenu() {
+        System.out.println("\n========================================");
+        System.out.println("Gym System");
+        System.out.println("========================================");
+        System.out.println("1. Add Member");
+        System.out.println("2. View All Members");
+        System.out.println("3. Add Trainer");
+        System.out.println("4. View All Trainers");
+        System.out.println("0. Exit");
+        System.out.println("========================================");
+        System.out.print("Enter your choice: ");
+    }
 
-        System.out.println("\n-----SECTION 4: MEMBER UPGRADE-----");
-        m1.upgradeMembership(premium);
-        System.out.println("After upgrade: " + m1);
+    public static void addMember() {
+        Random random = new Random();
+        int Id = random.nextInt();
+        System.out.println("\n--- ADD MEMBERS ---");
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
 
-        System.out.println("\n-----SECTION 5: WORKOUT SESSION-----");
-        WorkoutSession s1 = new WorkoutSession(501, m1, "Trainer Dias", 45, false);
-        System.out.println(s1);
-        System.out.println("Can session start? " + s1.canStart());
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt();
 
-        s1.extend(15);
-        System.out.println("After extend: " + s1);
+        System.out.print("Enter active: (true/false): ");
+        boolean active = scanner.nextBoolean();
+        scanner.nextLine();
 
-        s1.complete();
-        System.out.println("After complete: " + s1);
-        System.out.println("Can session start now? " + s1.canStart());
+        System.out.print("Enter Membership: (basic or premium): ");
+        String membership = scanner.nextLine();
 
-        System.out.println("\n-----SECTION 6: EDGE CASE TEST-----");
-        Member inactiveMember = new Member(102, "Timur", 16, false, basic);
-        WorkoutSession s2 = new WorkoutSession(502, inactiveMember, "Trainer Amina", 30, false);
-        System.out.println(inactiveMember);
-        System.out.println(s2);
-        System.out.println("Can inactive member start session? " + s2.canStart());
+        Membership selected;
+        if(membership.equals("basic")){
+            selected = basic;
+        } else {
+            selected = premium;
+        }
+
+        Member member = new Member(Id, name, age, active, selected);
+        members.add(member);
+
+        System.out.println("\n Member added successfully!");
+    }
+
+    public static void viewAllMembers() {
+        System.out.println("\n========================================");
+        System.out.println(" ALL MENU ITEMS");
+        System.out.println("========================================");
+        if(members.isEmpty()) {
+            System.out.println("No members found.");
+            return;
+        }
+
+        System.out.println("Total members: " + members.size());
+        System.out.println();
+
+        for(int i = 0; i < members.size(); i++) {
+            Member m = members.get(i);
+
+            System.out.println((i + 1 ) + ". " + m);
+        }
+    }
+
+    public static void addTrainer() {
+        Random random = new Random();
+        int Id = random.nextInt();
+        System.out.println("\n--- ADD TRAINER ---");
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter years of experience: ");
+        int experience = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter specialization: ");
+        String specialization = scanner.nextLine();
+
+        System.out.print("Enter salary: ");
+        double salary = scanner.nextDouble();
+        scanner.nextLine();
+
+        Trainer trainer = new Trainer(Id, name, age, experience, specialization, salary);
+        trainers.add(trainer);
+
+        System.out.println("\n Trainer added successfully!");
+    }
+
+    private static void viewAllTrainers() {
+        System.out.println("\n========================================");
+        System.out.println("            ALL TRAINERS                ");
+        System.out.println("========================================");
+
+        if (trainers.isEmpty()) {
+            System.out.println("No trainers found.");
+            return;
+        }
+
+        System.out.println("Total trainers: " + trainers.size());
+        System.out.println();
+
+        for (int i = 0; i < trainers.size(); i++) {
+            Trainer t = trainers.get(i);
+            System.out.println((i + 1) + ". " + t);
+        }
     }
 }
+
---
+++ src/Trainer.java
@@ -0,0 +1,50 @@
+public class Trainer {
+    private int Id;
+    private String name;
+    private int age;
+    private int experienceYears;
+    private String specialization;
+    private double salary;
+
+    public Trainer(int Id, String name, int age, int experienceYears, String specialization, double salary) {
+        this.Id = Id;
+        this.name = name;
+        this.age = age;
+        this.experienceYears = experienceYears;
+        this.specialization = specialization;
+        this.salary = salary;
+
+    }
+
+    public int getId() { return Id; }
+    public void setId(int Id) { this.Id = Id; }
+
+    public double getSalary() { return salary; }
+    public void setSalary(double salary) {
+        if (salary > 0) {
+            this.salary = salary;
+        }
+    }
+
+    public boolean isSenior() {
+        return experienceYears >= 5;
+    }
+
+    public void giveRaise(double percent) {
+       double newSalary =  salary * (1 + percent / 100);
+       setSalary(newSalary);
+    }
+
+    @Override
+    public String toString() {
+        return "Trainer{" +
+                "trainerId=" + Id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", experienceYear=" + experienceYears +
+                ", specialization=" + specialization +
+                ", salary=" + salary +
+                '}';
+    }
+
+}
